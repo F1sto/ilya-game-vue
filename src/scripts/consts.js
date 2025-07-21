2c21db556fc16a99cbaa2132480b89e81cd79dbc
andrei misyurin <andrew.misiyrin@gmail.com>
2025-07-21 13:27:58 +0600

    first commit

---
+++ src/scripts/consts.js
@@ -0,0 +1,49 @@
+export const AGES = {
+  young: `young`,
+  old: `old`,
+  adult: `adult`,
+};
+
+export const warehouse = {
+  baralgin: {
+    price: 9,
+    name: `baralgin`,
+  },
+  trigande: {
+    price: 6,
+    name: `trigande`,
+  },
+}
+
+export const medicals = {
+  baralgin: {
+    price: 10,
+    name: `baralgin`,
+    volume: 10,
+  },
+  trigande: {
+    price: 10,
+    name: `trigande`,
+    volume: 10,
+  },
+};
+
+
+export const clients = {
+  Oleg: {
+    name: `Oleg`,
+    age: AGES.young
+  },
+  Semen: {
+    name: `Semen`,
+    age: AGES.old
+  },
+  Kate: {
+    name: `Kate`,
+    age: AGES.young
+  },
+  Bob: {
+    name: `Bob`,
+    age: AGES.old
+  },
+};
